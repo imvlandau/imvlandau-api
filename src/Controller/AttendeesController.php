c04215da1029c31e765c4f0583613b5f1b3e2b13
imvlandau <itimvlandau@gmail.com>
2021-07-02 00:15:49 +0200

    use qrcode bundle and add/use token generator

--- src/Controller/AttendeesController.php
+++ src/Controller/AttendeesController.php
@@ -6,6 +6,7 @@ use App\Entity\Attendees;
 use App\Exception\ApiProblem;
 use App\Exception\ApiProblemException;
 use App\Repository\AttendeesRepository;
+use App\Utils\RandomStringGenerator;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use FOS\RestBundle\Controller\FOSRestController;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
@@ -14,8 +15,10 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 use Symfony\Contracts\Translation\TranslatorInterface;
-// use Endroid\QrCode\Builder\BuilderInterface;
-// use Endroid\QrCodeBundle\Response\QrCodeResponse;
+use Endroid\QrCode\Builder\BuilderInterface;
+use Endroid\QrCodeBundle\Response\QrCodeResponse;
+use Endroid\QrCode\Label\Alignment\LabelAlignmentCenter;
+use Endroid\QrCode\Label\Font\NotoSans;
 
 /**
  * Controller to manage attendees
@@ -25,11 +28,14 @@ use Symfony\Contracts\Translation\TranslatorInterface;
 class AttendeesController extends FOSRestController
 {
     public function __construct(
-        TranslatorInterface $translator
-        // , BuilderInterface $customQrCodeBuilder
+        TranslatorInterface $translator,
+        RandomStringGenerator $randomStringGenerator,
+        BuilderInterface $customQrCodeBuilder
     ) {
         $this->translator = $translator;
-        // $this->customQrCodeBuilder = $customQrCodeBuilder;
+        $this->customQrCodeBuilder = $customQrCodeBuilder;
+        $this->randomStringGenerator = $randomStringGenerator;
+        $this->randomStringGenerator->setAlphabet('2346789');
     }
 
     /**
@@ -51,40 +57,78 @@ class AttendeesController extends FOSRestController
      *
      * @return Response
      */
-    public function create(Request $request, ValidatorInterface $validator)
-    {
+    public function create(Request $request,ValidatorInterface $validator) {
         try {
-            $attendees = new Attendees();
-            $attendees->setName($request->request->get('name'));
-            $attendees->setEmail($request->request->get('email'));
-            $attendees->setMobile($request->request->get('mobile'));
-            $attendees->setCompanion_1($request->request->get('companion_1'));
-            $attendees->setCompanion_2($request->request->get('companion_2'));
-            $attendees->setCompanion_3($request->request->get('companion_3'));
-            $attendees->setCompanion_4($request->request->get('companion_4'));
+            $name = $request->request->get('name');
+            $email = $request->request->get('email');
+            $mobile = $request->request->get('mobile');
+            $companions = 0;
+            $companion1 = $request->request->get('companion_1');
+            $companion2 = $request->request->get('companion_2');
+            $companion3 = $request->request->get('companion_3');
+            $companion4 = $request->request->get('companion_4');
 
-            $errors = [];
-            $constraintValidator = $validator->validate($attendees, null, ['create']);
-            if (count($constraintValidator) > 0) {
-                foreach ($constraintValidator->getIterator() as $error) {
-                    $errors[] = $this->translator->trans($error->getMessage());
-                }
-                return new JsonResponse($errors, Response::HTTP_BAD_REQUEST);
+return $this->randomStringGenerator->generate(5);
+
+$attendeesRepository = $this->getDoctrine()->getRepository(Attendees::class);
+$ret = $attendeesRepository->findOneByEmailHash("989abcfa23e291f53caaaf059c81a8e0");
+return $ret;
+echo "<pre>" . print_r($ret, 1) . "</pre>";
+exit;
+// return $this->handleView($this->view(['status' => 'ok'], Response::HTTP_CREATED));
+
+// return $attendeesRepository->findAll();
+
+            if (!empty($companion1)){
+              $companions++;
+            }
+            if (!empty($companion2)){
+              $companions++;
+            }
+            if (!empty($companion3)){
+              $companions++;
+            }
+            if (!empty($companion4)){
+              $companions++;
             }
 
-            // save user provided public key
-            $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->persist($attendees);
-            $entityManager->flush();
+            $attendees = new Attendees();
+            $attendees->setName($name);
+            $attendees->setEmail($email);
+            $attendees->setMobile($mobile);
+            $attendees->setCompanion1($companion1);
+            $attendees->setCompanion2($companion2);
+            $attendees->setCompanion3($companion3);
+            $attendees->setCompanion4($companion4);
 
-  // $result = $this->customQrCodeBuilder
-  //         ->size(400)
-  //         ->margin(20)
-  //         ->build();
-  // $response = new QrCodeResponse($result);
-  // return $response;
+//             $errors = [];
+//             $constraintValidator = $validator->validate($attendees, null, ['create']);
+//             if (count($constraintValidator) > 0) {
+//                 foreach ($constraintValidator->getIterator() as $error) {
+//                     $errors[] = $this->translator->trans($error->getMessage());
+//                 }
+//                 return new JsonResponse($errors, Response::HTTP_BAD_REQUEST);
+//             }
+//
+//             // save user provided public key
+//             $entityManager = $this->getDoctrine()->getManager();
+//             $entityManager->persist($attendees);
+//             $entityManager->flush();
+//
+// $label = "";
+// if (!empty($companions)){
+//   $label = "+ $companions";
+// }
+// $result = $this->customQrCodeBuilder
+//   ->data("$name $label")
+//   ->labelText("$name $label")
+//   ->labelFont(new NotoSans(13))
+//   ->labelAlignment(new LabelAlignmentCenter())
+//   ->build();
+// $response = new QrCodeResponse($result);
+// return $response;
 
-  return $this->handleView($this->view(['status' => 'ok'], Response::HTTP_CREATED));
+return $this->handleView($this->view(['status' => 'ok'], Response::HTTP_CREATED));
 
 
         } catch (\Throwable $e) {
